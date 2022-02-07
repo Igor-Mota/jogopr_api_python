da53ac88c9b42220d37301ee14fd3d41ec911289
twixtaxe <igormota999@gmail.com>
2022-02-07 13:41:21 -0300

    add teacher whit email validationa

--- src/models/teachers_model.py
+++ src/models/teachers_model.py
@@ -3,10 +3,10 @@ from .connection import connect
 from mysql.connector import Error
 
 
-def get_a_teacher(name):
+def get_a_teacher(email):
     con = connect()
     cursor = con.cursor()
-    sql__check = str('select * from teachers where name = "{}"').format(name)
+    sql__check = str('select * from teachers where email = "{}"').format(email)
     cursor.execute(sql__check)
     records = cursor.fetchall()
 
@@ -20,7 +20,7 @@ def create_new_teacher(name, password_hash, email):
     sql_insert = 'INSERT INTO teachers (name, password, email) VALUES (%s, %s, %s)'
     values = (name, password_hash, email)
     try:
-        verify = get_a_teacher(name)
+        verify = get_a_teacher(email)
         if len(verify) > 0:
             return 'Usuario ja existe'
         else:
@@ -28,7 +28,7 @@ def create_new_teacher(name, password_hash, email):
             con.commit()
             cursor.close()
             con.close()
-            return get_a_teacher(name)
+            return get_a_teacher(email)
     except Error as error:
         return 'Error' + error
 
