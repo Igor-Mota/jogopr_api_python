9b9aab1c864f8936576dcfb920aa34b92ca0688c
twixtaxe <igormota999@gmail.com>
2022-02-07 11:55:06 -0300

    add cors

--- src/routes.py
+++ src/routes.py
@@ -1,13 +1,16 @@
 from .controllers import authenticate
 from flask import request, jsonify
+from flask_cors import CORS, cross_origin
 
 
 def routes(app):
     @app.route('/auth/register', methods=['POST'])
+    @cross_origin(origin='*', headers=['Content-Type', 'application/json'])
     def register():
         body = request.get_json()
         return jsonify(authenticate.register(body))
 
     @app.route('/list', methods=['GET'])
+    @cross_origin(origin='*', headers=['Content-Type', 'application/json'])
     def list_teachers():
         return jsonify(authenticate.get_all_teachers())
